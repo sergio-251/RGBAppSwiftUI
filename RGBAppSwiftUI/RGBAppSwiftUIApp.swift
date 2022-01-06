a00d061869f74b89dbbaf250dcc310a82556674a
EfimovSchool <sergio-251@yandex.ru>
2022-01-06 16:28:22 +0300

    Initial Commit

---
+++ RGBAppSwiftUI/RGBAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppSwiftUIApp.swift
+//  RGBAppSwiftUI
+//
+//  Created by Sergey Efimov on 06.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
